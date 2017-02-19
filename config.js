59dd47a9ae076dbf09f6c37644eadde876e604ee
root <root@Ghost-on-1404.052316-305>
2017-02-19 15:23:34 -0500

    come on

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ config = {
     // When running Ghost in the wild, use the production environment.
     // Configure your URL and mail settings here
     production: {
-        url: 'http://thinkgrenada.xyz',
+        url: 'http://www.thinkgrenada.xyz',
         mail: {
 	transport: 'SMTP',
 		options: {
